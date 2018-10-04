4de4aa69dec8207c8557c8aaab88d6d30a65a4d6
Jarno Elovirta <jarno@elovirta.com>
2018-10-04 10:38:33 +0300

    Add basic plugin metadata structure validation

--- .travis/validate.js
+++ .travis/validate.js
@@ -1,10 +1,42 @@
-#!/usr/bin/env node
-
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const fs = require("fs");
+const readFileAsync = util.promisify(fs.readFile);
 
 async function changedFiles() {
-    const { stdout, stderr } = await exec(`git diff --name-only HEAD...${process.env.TRAVIS_BRANCH}`);
-    console.log(stdout.split(/\r?\n/));
+  const { stdout, stderr } = await exec(
+    `git diff --name-only ${process.env.TRAVIS_BRANCH}...HEAD`
+  );
+  return stdout
+    .trim()
+    .split(/\r?\n/)
+    .filter(file => file.match(/.json$/));
 }
-changedFiles();
+
+function validate(plugins) {
+  plugins.forEach(plugin => {
+    if (!plugin.name) {
+      throw new Error("plugin name missing");
+    }
+    if (!plugin.vers) {
+      throw new Error("plugin vers missing");
+    }
+    if (!plugin.url) {
+      throw new Error("plugin url missing");
+    }
+  });
+}
+
+changedFiles().then(files => {
+  files.map(file => {
+    readFileAsync(file, { encoding: "utf8" }).then(data => {
+      const plugin = JSON.parse(data);
+      try {
+        validate(plugin);
+      } catch (e) {
+        console.error(`ERROR: Plugin ${file} validation failed: ${e.message}: ${JSON.stringify(plugin)}`);
+        process.exit(1);
+      }
+    });
+  });
+});

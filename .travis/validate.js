0a28ae77cbd2452c4a8c0ee396c5bccf3b9c52f5
Jarno Elovirta <jarno@elovirta.com>
2018-11-10 15:11:48 +0200

    Validate only changed versions
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- .travis/validate.js
+++ .travis/validate.js
@@ -1,33 +1,60 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
-const fs = require("fs");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs');
 const readFileAsync = util.promisify(fs.readFile);
 
 async function changedFiles() {
-  const { stdout, stderr } = await exec(
-    `git diff --name-only ${process.env.TRAVIS_BRANCH}...HEAD`
-  );
+  const { stdout, stderr } = await exec(`git diff --name-only ${process.env.TRAVIS_BRANCH}...HEAD`);
   return stdout
     .trim()
     .split(/\r?\n/)
     .filter(file => file.match(/.json$/));
 }
 
-function validate(plugins) {
+async function readBaseFile(file, opts) {
+  const { stdout, stderr } = await exec(`git show ${process.env.TRAVIS_BRANCH}:${file}`, { encoding: 'utf8' });
+  return stdout;
+}
+
+function arrayToMapByVersion(plugins) {
+  const res = {};
+  plugins.forEach(plugin => {
+    res[plugin.vers] = plugin;
+  });
+  return res;
+}
+
+function isSame(p1, p2) {
+  if (!p1 || !p2) {
+    return false;
+  }
+  return !['name', 'url', 'cksum'].map(key => p1[key] === p2[key]).includes(false);
+}
+
+function validate(plugins, origs) {
   if (!!plugins.alias) {
     // FIXME
     return;
   }
+  const origByVers = arrayToMapByVersion(origs);
   plugins.forEach(plugin => {
+    if (isSame(plugin, origByVers[plugin.vers])) {
+      console.log(`INFO: No changes in ${plugin.name}@${plugin.vers}`);
+      return;
+    }
+
     console.log(`INFO: Validating plugin ${plugin.name}@${plugin.vers}`);
     if (!plugin.name) {
-      throw new Error("plugin name missing");
+      throw new Error('plugin name missing');
     }
     if (!plugin.vers) {
-      throw new Error("plugin vers missing");
+      throw new Error('plugin vers missing');
     }
     if (!plugin.url) {
-      throw new Error("plugin url missing");
+      throw new Error('plugin url missing');
+    }
+    if (!plugin.cksum) {
+      console.log('WARN: cksum missing');
     }
   });
 }
@@ -35,18 +62,15 @@ function validate(plugins) {
 changedFiles()
   .then(files => {
     files.map(file => {
-      readFileAsync(file, { encoding: "utf8" })
-        .then(data => {
+      Promise.all([readFileAsync(file, { encoding: 'utf8' }), readBaseFile(file)])
+        .then(([data, orig]) => {
           console.log(`INFO: Reading ${file}`);
           const plugin = JSON.parse(data);
+          const origPlugin = JSON.parse(orig);
           try {
-            validate(plugin);
+            validate(plugin, origPlugin);
           } catch (e) {
-            console.error(
-              `ERROR: Plugin ${file} validation failed: ${
-                e.message
-              }: ${JSON.stringify(plugin)}`
-            );
+            console.error(`ERROR: Plugin ${file} validation failed: ${e.message}: ${JSON.stringify(plugin)}`);
             process.exit(1);
           }
         })

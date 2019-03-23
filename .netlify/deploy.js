f60d3e132b1d2f3047042eea81a682b295072882
Jarno Elovirta <jarno@elovirta.com>
2019-03-23 09:06:40 +0200

    Minimize metadata files and add logging
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- .netlify/deploy.js
+++ .netlify/deploy.js
@@ -23,15 +23,22 @@ async function listFiles() {
   const all = {};
   const index = [];
   for (let file of files) {
-    await fs.copyFile(`${src}/${file}`, `${site}/${file}`);
-    const data = await fs.readFile(`${src}/${file}`, { encoding: 'utf8' });
+    const input = `${src}/${file}`;
+    const output = `${site}/${file}`;
+    const data = await fs.readFile(input, { encoding: 'utf8' });
     const json = JSON.parse(data);
+    console.log(`Write ${output}`);
+    await fs.writeFile(output, JSON.stringify(json), { encoding: 'utf8' });
     const id = file.substring(0, file.length - 5);
     all[id] = json;
     index.push(id);
   }
-  await fs.writeFile(`${site}/_all.json`, JSON.stringify(all), { encoding: 'utf8' });
-  await fs.writeFile(`${site}/_index.json`, JSON.stringify(index), { encoding: 'utf8' });
+  const allOutput = `${site}/_all.json`
+  console.log(`Write ${allOutput}`);
+  await fs.writeFile(`${allOutput}`, JSON.stringify(all), { encoding: 'utf8' });
+  const indexOutput = `${site}/_index.json`
+  console.log(`Write ${indexOutput}`);
+  await fs.writeFile(`${indexOutput}`, JSON.stringify(index), { encoding: 'utf8' });
 }
 
 listFiles();

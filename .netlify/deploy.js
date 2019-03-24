99f7cedb7c1f215d6a87164029238d3c9d25ba1f
Jarno Elovirta <jarno@elovirta.com>
2019-03-24 07:31:01 +0200

    Refactor and clean
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- .netlify/deploy.js
+++ .netlify/deploy.js
@@ -8,37 +8,39 @@ const fs = {
   writeFile: util.promisify(_fs.writeFile)
 };
 
-async function listFiles() {
-  const src = '..';
-  const site = 'build';
+const SRC = '..';
+const BUILD = 'build';
+const PREFIX = '.json'
+
+async function main() {
   try {
-    await fs.mkdir(site, { recursive: true });
+    await fs.mkdir(BUILD, { recursive: true });
   } catch (err) {
     if (err.code !== 'EEXIST') {
       throw err;
     }
   }
-  const filesAll = await fs.readdir(src);
-  const files = filesAll.filter(file => file.endsWith('.json') && !file.startsWith('_'));
+  const filesAll = await fs.readdir(SRC);
+  const files = filesAll.filter(file => file.endsWith(PREFIX) && !file.startsWith('_'));
   const all = {};
   const index = [];
   for (let file of files) {
-    const input = `${src}/${file}`;
-    const output = `${site}/${file}`;
+    const input = `${SRC}/${file}`;
+    const output = `${BUILD}/${file}`;
     const data = await fs.readFile(input, { encoding: 'utf8' });
     const json = JSON.parse(data);
     console.log(`Write ${output}`);
     await fs.writeFile(output, JSON.stringify(json), { encoding: 'utf8' });
-    const id = file.substring(0, file.length - 5);
+    const id = file.substring(0, file.length - PREFIX.length);
     all[id] = json;
     index.push(id);
   }
-  const allOutput = `${site}/_all.json`
+  const allOutput = `${BUILD}/_all.json`
   console.log(`Write ${allOutput}`);
   await fs.writeFile(`${allOutput}`, JSON.stringify(all), { encoding: 'utf8' });
-  const indexOutput = `${site}/_index.json`
+  const indexOutput = `${BUILD}/_index.json`
   console.log(`Write ${indexOutput}`);
   await fs.writeFile(`${indexOutput}`, JSON.stringify(index), { encoding: 'utf8' });
 }
 
-listFiles();
+main();
